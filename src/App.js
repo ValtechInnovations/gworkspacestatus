68c0048f72e577404928b9673c60ea71c0614742
ValtechInnovations <138601237+ValtechInnovations@users.noreply.github.com>
2024-03-09 09:17:03 +0530

    useEffect , getStatusIcon and HTML stucture updated - 02

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [productData, setProductData] = useState([]);
+  
+  
+      useEffect(() => {
+          fetch('https://gworkspacestatus.pythonanywhere.com/get_json')
+              .then(response => response.json())
+              .then(data => {
+                  const productsToDisplay = ["Classroom", "Gmail", "Google Calendar", "Google Chat", "Google Docs", "Google Drive", "Google Meet", "Google Sheets", "Google Slides"];
+                  const filteredData = data.filter(product => productsToDisplay.includes(product.product_name));
+                  setProductData(filteredData);
+              })
+              .catch(error => console.error('Error fetching data:', error));
+      }, []);
+
+      
+          const getStatusIcon = (statusContainerLabel, endMarkerLabel) => {
+              if (statusContainerLabel === "Disruption status") {
+                  return <img src="cross_icon.png" className="icon" alt="Disruption" />;
+              } else if (statusContainerLabel === "Available status" && endMarkerLabel === "Available status") {
+                  return <img src="check_icon.png" className="icon" alt="Available" />;
+              } else {
+                  return null; // No icon
+              }
+          };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container"> {/* Wrap the table in a container for Bootstrap styling */}
+    <h1 className="mt-4 mb-3">Product Status</h1> {/* Add margin to the heading */}
+    <table className="table"> {/* Add Bootstrap table class */}
+      <thead className="thead-dark"> {/* Add dark background to the table header */}
+        <tr>
+          <th>Product Name</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {productData.map(product => (
+          <tr key={product.product_name}>
+            <td>{product.product_name}</td>
+            <td>{getStatusIcon(product.status_container_label, product.end_marker_label)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
   );
 }
 
